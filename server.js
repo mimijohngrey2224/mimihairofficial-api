5737a68c40becb9578e538708ea55c0cbaeaa58f
MIMi <mimijohngrey@outlook.com>
2026-07-14 14:31:26 +0100

    urll

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(cors({
     // origin: "http://localhost:5173",
-    origin:"https://mimihairofficial-frontend.vercel.app/",
+    origin:"https://mimihairofficial-frontend.vercel.app",
     allowedHeaders: ["Content-Type", "Authorization", "auth-token"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
      credentials: true 
